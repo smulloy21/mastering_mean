3470c2bee60e16602858186bd3f8ec9948a57e6c
Shannon Mulloy <student@epicodus.com>
2015-12-03 13:41:08 -0800

    create ejs view

--- config/express.js
+++ config/express.js
@@ -6,7 +6,6 @@ var express = require('express'),
 
 module.exports = function() {
   var app = express();
-
   if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
   } else if (process.env.NODE_ENV === 'production') {
@@ -18,6 +17,9 @@ module.exports = function() {
   }));
   app.use(bodyParser.json());
   app.use(methodOverride());
+
+  app.set('views', './app/views');
+  app.set('view engine', 'ejs');
 
   require('../app/routes/index.js')(app);
   return app;
